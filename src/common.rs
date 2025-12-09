0034a33305950ba07eaf8f62301be42dc7cb0ac1
YaoC1zhiC <791256109@qq.com>
2025-12-09 10:10:52 +0800

    更新配置文件

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https:// 192.168.100.248".to_owned()
 }
 
 #[inline]
